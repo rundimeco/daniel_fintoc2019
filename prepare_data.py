16cd724dc43ba92a3c3b4e8258c34611cd847773
rundimeco <rundimeco@gmail.com>
2019-10-25 17:06:12 +0200

    JPT Thx

--- prepare_data.py
+++ prepare_data.py
@@ -3,6 +3,7 @@ import re, json
 
 data_csv = sys.argv[1]
 
+print ("Input : %s"%data_csv)
 f = open(data_csv)
 lines = f.readlines()
 f.close()
@@ -14,6 +15,8 @@ for cpt, l in enumerate(lines[1:]):
   elems = re.split("\t", re.sub("\n", "", l))
   infos = {attributes[i]: elems[i] for i in range(len(attributes))}
   dic[cpt] = infos
-w = open("%s.json"%data_csv, "w")
+out_name = "%s.json"%data_csv
+print ("Output : %s"%out_name)
+w = open(out_name, "w")
 w.write(json.dumps(dic, indent = 2))
 w.close()
