05c7be1e850d8087bdf40a0c8d5fe4c2288bb807
jbtanguy <jbtanguy56@gmail.com>
2019-10-29 12:09:29 +0100

    Ajout de commentaires

--- baselines.py
+++ baselines.py
@@ -62,43 +62,54 @@ def prepare_data(json_path, mode):
 def vectorize(options):
   if options.verbose==True:  print(options)
   liste_modes = ["B1", "B2", "B3", "B4", "B5", "B6"]
+  # On prend connaissance des types de modèles à apprendre
   if options.mode!="all":
     liste_modes = [options.mode]
+
+  # Pour chaque type de modèle à apprendre et tester...
   for mode in liste_modes:
+    # Création des ensembles de train et de test
     dic_out = {"train":{}, "test":{}}
     desc = {}
-
     X_train, y_train, IDs_train = prepare_data(options.train, mode)
     X_test, y_test, IDs_test = prepare_data(options.test, mode)
-    liste_classif = get_liste_classif()
-    skf = StratifiedKFold(10, shuffle=False)
 
+    # On récupère les classifieurs à utiliser (ils sont instanciés dans la fonction get_liste_classif())
+    liste_classif = get_liste_classif()
+    # Cross-validation
+    skf = StratifiedKFold(10, shuffle=False)
+    # Info et dossier nécessaire à la suite
     print("%s--mode: %s--%s"%("="*10, mode, "="*10))
     dataset_name = format_name(options.train, options.test)
     mkdirs("results_B_final/%s/"%(dataset_name))
 
+    # Pour chaque classifieur...
     for name_classif, OBJ in liste_classif:
       print("%s%s%s"%("-"*10,name_classif,"-"*10))
       config = "%s__%s.txt"%(mode,name_classif)
-      out_name ="results_B_final/%s/%s"%(dataset_name, config )
-      if os.path.exists(out_name) and options.force==False:
+      out_name ="results_B_final/%s/%s"%(dataset_name, config)
+      if os.path.exists(out_name) and options.force==False: # Si on a déjà fait le travail (et qu'on n'a pas -f en argument), on passe
         print("Already DONE")
         continue
 
+      # Accuracy sur l'ensemble TRAIN en cross validation
       accuracy = np.mean(cv_score(OBJ, X_train,y=y_train, cv=skf, n_jobs=2))
-      model = OBJ.fit(X_train, y_train)
-      y_pred = model.predict(X_test)
-      
-      model_name = "models/%s.sav"%config
-      pickle.dump(model, open(model_name, 'wb')) 
 
+      # Apprentissage du classifieur sur le TRAIN et métrique sur le TEST
+      model = OBJ.fit(X_train, y_train) # apprentissage
+      y_pred = model.predict(X_test) # prédiction sur le test
+      model_name = "models/%s.sav"%config
+      pickle.dump(model, open(model_name, 'wb')) # Sauvegarde du modèle
+      # Récupération des performances
       score_test = get_score(y_test, y_pred)
       if options.verbose==True: print(out_name)
       y_pred_out = format_output(IDs_test, y_pred)
+      
+      # Ecriture et affichage des performances du modèle
       write_utf8(out_name, (y_pred_out))
       print("Acc. (cross-valid)\t: %f"%accuracy)
       print("Acc. (test)\t\t: %f"%(score_test))
 
 if __name__=="__main__":    
-  options = get_args_BL()
-  vectorize(options)
+  options = get_args_BL() # Parse des arguments
+  vectorize(options) # Appel de la fonction principale avec les options demandées en ligne de commande
