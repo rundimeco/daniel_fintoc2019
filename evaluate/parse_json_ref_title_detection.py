62758d739afd6b2e32cb45d50aa3eeb9a2794db7
jbtanguy <jbtanguy56@gmail.com>
2019-10-29 11:48:51 +0100

    Suppression dun fichier inutile.

--- evaluate/parse_json_ref_title_detection.py
+++
@@ -1,8 +0,0 @@
-import json
-
-test_set = json.load(open('./../data/shared_task_2.test.csv.json', 'r'))
-
-for id in test_set.keys(): 
-	for att in test_set[id].keys():
-		if att == 'label':
-			print(id + '\t' + test_set[id][att])
